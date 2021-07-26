74e71188c30ca854a6371cec1c5abd3f443f8437
cflin <cflin@mail.cjcu.edu.tw>
2021-07-26 14:09:04 +0800

    我修改test3讓BGR轉RGB

--- test3.py
+++ test3.py
@@ -3,6 +3,7 @@ import cv2 as cv
 from matplotlib import pyplot as plt
 
 img = cv.imread('./images/test1.jpg')
-plt.imshow(img, interpolation = 'bicubic')
-# plt.xticks([]), plt.yticks([]) # 隐藏 X 和 Y 轴的刻度值
+img2 = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+plt.imshow(img2, interpolation = 'bicubic')
+# plt.xticks([]), plt.yticks([]) # 隐藏 X 和 Y 轴的刻度
 plt.show()
